56b24589cab7f65c44c65caf84ec236fa03d0bdb
Matsusho0825 <sho.matsuda0825@gmail.com>
2023-11-13 15:57:10 +0900

    feat/POST-endpoint

--- spec/spec.js
+++ spec/spec.js
@@ -24,10 +24,25 @@ describe("The express server", () => {
           const res = await request.get("/api/fishes");
           const expected = [
             {"id": 1, "name": "sake", "habitat": "japan", "cost_price": "100.00"},
-            {"id": 2, "name": "hokke", "habitat": "finland", "cost_price": "200.00"}
+            {"id": 2, "name": "hokke", "habitat": "finland", "cost_price": "200.00"},
           ];
           JSON.parse(res.text).should.deep.equal(expected);
         });
       });
+      describe("POST /api/fishes - register fishes", () => {
+        it("should register all fishes", async () => {
+          await request.post("/api/fishes").send({"fish": {"id": 3, "name": "saba", "habitat": "china", "cost_price": "300.00"}});
+          console.log("Insert Complete");
+        });
+        it("should return inserted fishes", async () => {
+            const res = await request.get("/api/fishes");
+            const expected = [
+                {"id": 1, "name": "sake", "habitat": "japan", "cost_price": "100.00"},
+                {"id": 2, "name": "hokke", "habitat": "finland", "cost_price": "200.00"},
+                {"id": 3, "name": "saba", "habitat": "china", "cost_price": "300.00"},
+              ];
+            JSON.parse(res.text).should.deep.equal(expected);
+          });
+        });
+      });
    })
-})
--- src/db/osakana/osakana.controller.js
+++ src/db/osakana/osakana.controller.js
@@ -1,8 +1,13 @@
 const osakanaModel = require("./osakana.model");
 
+
 module.exports = {
   async index(req, res) {
     const fishes = await osakanaModel.getAll();
     res.json(fishes);
+  },
+  async register(req, res) {
+    const registeredFishId = await osakanaModel.register(req.body.fish);
+    res.send(registeredFishId);
   }
 }
